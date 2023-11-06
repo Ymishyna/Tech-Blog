b5157bb4b1c772721bcca01b6a72d024fb514cf7
Yana Mishyna <yana.mishina.92@gmail.com>
2023-11-06 14:57:16 -0500

    created api call for GET and POST

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -9,3 +9,33 @@ router.get('/', (req, res) => {
             res.status(500).json(err);
         })
 });
+
+//API call to GET all the comments 
+router.get('/:id', (req, res) => {
+    Comment.findAll({
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+//API call to POST the comment
+router.post('/', withAuth, (req, res) => {
+    if (req.session) {
+        Comment.create({
+                comment_text: req.body.comment_text,
+                post_id: req.body.post_id,
+                user_id: req.session.user_id,
+            })
+            .then(dbCommentData => res.json(dbCommentData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            })
+    }
+});
