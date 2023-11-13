f870ca7023cc8c168eb1fe08deb44aeb51c504e0
Yana Mishyna <yana.mishina.92@gmail.com>
2023-11-13 15:22:32 -0500

    adjusted models,based on a previos bug fix in controllers

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {
-    // set up method to run on instance data (per user) to check password 
+    // set up method to check password 
     checkPassword(loginPW) {
         return bcrypt.compareSync(loginPW, this.password);
     }
@@ -12,61 +12,56 @@ class User extends Model {
 
 // define table columns and configuration
 User.init(
-  {
-    // define an id column
-    id: {
-        // use the special Sequelize DataTypes object provide what type of data it is 
-        type: DataTypes.INTEGER,
-        // equivalent of SQL "NOT NULL"
-        allowNull: false,
-        // instruct that this is the Primary Key
-        primaryKey: true,
-        // turn on auto increment
-        autoIncrement: true
-    },
-    // define a username column
-    username: {
-        type: DataTypes.STRING,
-        allowNull:false,
-        unique: true
-    },
-    // define a password column 
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-            // this means the password must be at least four characters long
-            len: [4]
-        }
-    }
-  },
-  {
-    hooks: {
-        // set up beforeCreate lifecycle "hook" functionality
-        async beforeCreate(newUserData) {
-            newUserData.password = await bcrypt.hash(newUserData.password, 10);
-            return newUserData;
-            
+    {
+        // define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            // the same as in SQL "NOT NULL"
+            allowNull: false,
+            // this is the Primary Key
+            primaryKey: true,
+            // auto increment
+            autoIncrement: true
         },
-        // set up beforeUpdate lifecycle "hook" functionality
-        async beforeUpdate(updatedUserData) {
-            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-            return updatedUserData;
+        // defining username column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // defining password column 
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // password must be at least four characters long
+                len: [4]
+            }
         }
     },
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
 
-    // pass in our imported sequelize connection (the direct connection to our database)
-    sequelize,
-    // don't automatically create createdAt/updatedAt timestamp fields
-    timestamps: false,
-    // don't pluralize name of database table
-    freezeTableName: true,
-    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-    underscored: true,
-    // make it so our model name stays lowercase in the database
-    modelName: 'user'
-  }
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        // direct connection to our database
+        sequelize,
+        // don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        // don't pluralize name of db table
+        freezeTableName: true,
+        // use underscores instead of camel-casing 
+        underscored: true,
+        modelName: 'User'
+    }
 );
 
 module.exports = User;
