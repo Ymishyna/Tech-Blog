b1d81903226982b2385fa2cbb65da3cb601fd6d9
Yana Mishyna <yana.mishina.92@gmail.com>
2023-11-13 16:59:03 -0500

    added more atyling

--- utils/auth.js
+++ utils/auth.js
@@ -1,11 +1,10 @@
 const withAuth = (req, res, next) => {
-    // If the user is not signedin, redirect the request to the signin route
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
-  
+  // If the user is not signedin, redirect the request to the signin route
+  if (!req.session.userId) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
