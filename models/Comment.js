7d6f99f25ac8a7a9d54114f1ee053cb75cb25f3c
Yana Mishyna <yana.mishina.92@gmail.com>
2023-11-13 15:13:21 -0500

    fixed bugs in controllers

--- models/Comment.js
+++ models/Comment.js
@@ -1,45 +1,64 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-// Comment model
-class Comment extends Model { }
+// // Comment model
+// class Comment extends Model { }
+
+// Comment.init(
+//     {
+//         id: {
+//             type: DataTypes.INTEGER,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
+//         comment_text: {
+//             type: DataTypes.STRING,
+//             validate: {
+//                 len: [3]
+//             }
+//         },
+//         user_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//                 model: 'user',
+//                 key: 'id'
+//             }
+//         },
+//         post_id: {
+//             type: DataTypes.INTEGER,
+//             allowNull: false,
+//             references: {
+//                 model: 'post',
+//                 key: 'id'
+//             }
+//         }
+//     },
+//     {
+//         sequelize,
+//         freezeTableName: true,
+//         underscored: true,
+//         modelName: 'comment'
+//     }
+// );
+
+// module.exports = Comment;
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
+class Comment extends Model {}
 
 Comment.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        comment_text: {
-            type: DataTypes.STRING,
-            validate: {
-                len: [3]
-            }
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id'
-            }
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'post',
-                key: 'id'
-            }
-        }
-    },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'comment'
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
+  },
+  {
+    sequelize
+  }
 );
 
 module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -1,40 +1,57 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+// const { Model, DataTypes } = require('sequelize');
+// const sequelize = require('../config/connection');
 
-// Post model
+// // Post model
+// class Post extends Model {}
+
+// // fields/columns for Post model
+// Post.init(
+//     {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//         primaryKey: true,
+//         autoIncrement: true
+//       },
+//       title: {
+//         type: DataTypes.STRING,
+//         allowNull: false
+//       },
+//       content: {
+//         type: DataTypes.TEXT,
+//         allowNull: false
+//       },
+//       user_id: {
+//         type: DataTypes.INTEGER,
+//         references: {
+//           model: 'user',
+//           key: 'id'
+//         }
+//       }
+//     },
+//     {
+//       sequelize,
+//       freezeTableName: true,
+//       underscored: true,
+//       modelName: 'post'
+//     }
+//   );
+  
+//   module.exports = Post;
+
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/config');
+
 class Post extends Model {}
 
-// fields/columns for Post model
 Post.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
-    },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'post'
-    }
-  );
-  
-  module.exports = Post;
+  {
+    title: DataTypes.STRING,
+    body: DataTypes.STRING
+  },
+  {
+    sequelize
+  }
+);
+
+module.exports = Post;
